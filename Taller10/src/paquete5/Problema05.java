ccf4a21c6c602e8f90c60dc43dc931a4aa459437
Pablo Costa <pacosta2@utpl.edu.ec>
2021-12-31 01:25:34 -0500

    commit

--- Taller10/src/paquete5/Problema05.java
+++ Taller10/src/paquete5/Problema05.java
@@ -10,5 +10,30 @@ package paquete5;
  * @author reroes
  */
 public class Problema05 {
-    
+
+    public static void main(String[] args) {
+        String[][] estudiantes = {{"Sarah Ward", "Philip Payne"},
+        {"Carrie Burton", "Lauren Rice"},
+        {"Emma Escobar", "Lori Flores"},
+        {"Steven West", "Toni Martin"}
+        };
+        int fila;
+        int columna;
+        
+        for (fila = 0; fila < estudiantes.length; fila++) {
+
+            for (columna = 0; columna < estudiantes[fila].length; columna++) {
+
+                char inicial = estudiantes[fila][columna].charAt(0);
+                String inicialDos = String.valueOf(inicial);
+                if (inicialDos.equals("S") ||inicialDos.equals("P")||
+                        inicialDos.equals("T") ){ 
+                    
+                        System.out.println(estudiantes[fila][columna]);
+                
+
+                }
+            }
+        }
+    }
 }
--- Taller10/src/paquete6/Problema06.java
+++ Taller10/src/paquete6/Problema06.java
@@ -10,5 +10,31 @@ package paquete6;
  * @author reroes
  */
 public class Problema06 {
-    
-}
+
+    public static void main(String[] args) {
+
+        String[][] estudiantes = {{"Sarah Ward", "Philip Payne"},
+        {"Carrie Burton", "Lauren Rice"},
+        {"Emma Escobar", "Lori Flores"},
+        {"Steven West", "Toni Martin"}
+        };
+         int fila;
+        int columna;
+        
+        for (fila = 0; fila < estudiantes.length; fila++) {
+
+            for (columna = 0; columna < estudiantes[fila].length; columna++) {
+
+              
+                
+               // if (estudiantes.length = 11) { 
+                    
+                        System.out.println(estudiantes.length);
+                
+
+                }
+            }
+        }
+        
+    }
+
